07abe4ae9ad1747b375e98672ec455aa654a4abb
jasser <jasser.chaoued@outlook.com>
2021-09-09 00:15:18 +0100

    setting up componenets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,31 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   template: `
-    <p>
-      app works !
-      Cart: {{cart.length}}
+<div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <h1 class="text-center">Online Store</h1>
+        <h6 class="text-center"><a [routerLink]="['/cart']">Cart: {{(cart | async).length}}</a></h6>
+        <hr />
+      </div>
+    </div>
     <router-outlet></router-outlet>
-    </p>
-   
+  </div>
   `,
   styles: []
 })
 export class AppComponent {
   title = 'online-store-ngrx';
-  cart: Array<any>
+  cart: Observable<Array<any>>
 
   constructor(private store: Store<any>) {}
 
   ngOnInit() {
-    this.store.select('cart').subscribe((state => this.cart = state))
+    this.cart = this.store.select('cart')
   }
-}
+  }
+
